9909aef7fc5d6ad08e55b96a9070bf6eabbe7b98
massinissa CHALAL <chalal.massi0666@gmail.com>
2023-09-11 12:27:29 +0200

    initial commit

---
+++ src/test/java/fr/uparis/morpion/metamorpionback/MetaMorpionBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.uparis.morpion.metamorpionback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaMorpionBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
